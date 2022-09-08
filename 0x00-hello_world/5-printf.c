55a5f8b19476575cbaf8ed578671e178b2812022
Omolayo Temitayo <temitayoomolayo@gmail.com>
2022-09-08 14:52:40 -0700

    5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a place of art,\n");
+	return (0);
+}

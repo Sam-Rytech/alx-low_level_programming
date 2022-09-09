ac2837b3c45dafb18d674b08b4159aa81e56efb1
Omolayo Temitayo <temitayoomolayo@gmail.com>
2022-09-09 13:41:54 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,6 +5,7 @@
 /**
  * main - Entry point
  *
+ *
  * Return: always return 0
  *
  */
@@ -27,5 +28,5 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastn);
 	}
-	return(0);
+	return (0);
 }

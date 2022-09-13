3183b89c0330df39f830b9ae4aa42e16840a4261
Omolayo Temitayo <temitayoomolayo@gmail.com>
2022-09-13 12:46:57 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * Returns: always return 0
+ * Return: Always returns 0
  *
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

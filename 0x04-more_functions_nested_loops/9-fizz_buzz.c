530e869ca575b89eb68879f7036e6956e2b30453
Omolayo Temitayo <temitayoomolayo@gmail.com>
2022-09-15 15:07:02 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,25 @@ int main(void)
 {
 	for (num = 1; num <= 100; num++)
 	{
-	if ((num % 3 == 0) && (num % 5 == 0))
-	{
-		printf("FizzBuzz")
-	}
-	else if (num % 3 == 0)
-	{
-		printf("Fizz")
-	}
-	else if (num % 5 == 0)
-	{
-		printf("Buzz")
-	}
-	else
-	{
-		printf("%d", num)
-	}
-	if (i != 100)
-		printf(" ");
-	}
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz")
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz")
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz")
+		}
+		else
+		{
+			printf("%d", num)
+		}
+		if (i != 100)
+			printf(" ");
+		}
 	printf("\n");
 	return (0);
 }
